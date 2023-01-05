964bc434d1076d343db97435a3dc34c8a0414b85
l_inzirillo <l_inzirillo@etu-webschoolfactory.fr>
2023-01-05 20:35:30 +0100

    Creation of server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: false,
+      playground: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
