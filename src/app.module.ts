589654fcd4438cf4860cf88e65f28e9740e14028
Ludovic <ludoviclelong10@gmail.com>
2023-01-07 16:45:55 +0100

    J'ai supprimé tout ce qu'on avait sur GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
 import { join } from 'path';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      debug: false,
-      playground: false,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
