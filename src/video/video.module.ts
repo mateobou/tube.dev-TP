c3f78c76efb04a01bf216d4eaf09842cb3be3143
Ludovic <ludoviclelong10@gmail.com>
2023-01-12 20:20:37 +0100

    sauvegarde pour ne pas perdre

--- src/video/video.module.ts
+++ src/video/video.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // import { VideoController } from './video/video.controller';
 import { Video } from '../video/video.entity';
-
+import { VideoService } from '../video/video.service';
+import { VideoController } from '../video/video.controller';
 
 
 
 @Module({
   imports: [
    TypeOrmModule.forFeature([Video]),
+   providers: [VideoService],
+   controllers: [VideoController],
   ],
   providers: [],
   controllers: [],
