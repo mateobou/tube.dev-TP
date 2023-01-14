19f0e63f1c95e02092d7cda1c3d67579c6b14055
mateobou <46542381+mateobou@users.noreply.github.com>
2023-01-14 14:53:18 +0100

    [Edit] Bug correction - adding the UsesrModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserService } from './user/user.serivce';
-import { UsersController } from './user/users.controller';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserService } from "./user/user.serivce";
+import { UsersController } from "./user/users.controller";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserRepository } from "./user/user.repository";
+import { UsersModule } from "./user/users.module";
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://usertest:test@cluster0.m4mrrwq.mongodb.net/test',
+      "mongodb+srv://protube:test@cluster0.erzbctl.mongodb.net/test",
     ),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
