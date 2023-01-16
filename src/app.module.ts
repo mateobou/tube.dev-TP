8b912426daf77ccc42c19e347847dfae9e421b7b
mateobou <46542381+mateobou@users.noreply.github.com>
2023-01-16 19:59:52 +0100

    [Edit] Connection to Mongodb explaination and explaination of how to add the videoModule to the app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Video } from './video/video.entity';
 import { VideoModule } from './video/video.module';
 
-
 @Module({
   imports: [
+    /*
+     MongooseModule.forRoot(
+      "mongodb+srv://protube:test@cluster0.erzbctl.mongodb.net/test",
+    ),
+    //Ce code permet de se connecter à la bdd qu'a créé Luca. 
+    //Pour le moment seulement Luca y a accès mais tu peux temporairement créer la tienne avec MongoDb Compass et en te connectant sur le site de
+    //MongoDb 
+    */
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
@@ -23,10 +30,9 @@ import { VideoModule } from './video/video.module';
       retryAttempts: 5,
       autoLoadEntities: true,
     }),
+    VideoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-
-
 export class AppModule {}
