26233971807256645fc6d59eef4a8916f249efbc
Ludovic <ludoviclelong10@gmail.com>
2023-02-04 10:42:38 +0100

    1er test retourne Hello world à la racine modifié et fait

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,21 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
+
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
+beforeEach(() => {
+  appService = new AppService();
+  appController = new AppController(appService);
+});
+
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('Devrait retourner "Hello World!" à la racine du site', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
