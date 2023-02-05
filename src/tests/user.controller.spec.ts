f96fc32c275fb3754ececda99643893b15f806e9
Ludovic <ludoviclelong10@gmail.com>
2023-02-05 10:14:50 +0100

    1er test pour les users type du login fait, je vais le tester dans la branche develop

--- src/tests/user.controller.spec.ts
+++ src/tests/user.controller.spec.ts
@@ -1,23 +1,45 @@
-import { UserController } from '../user/app.controller';
-import { AppService } from '../user/app.service';
-
-/* test qui réussit si à la racine du site, est affiché 'Hello World!' */
-
-describe('AppController', () => {
-  let userController: UserController;
-  let userService: UserService;
+import { Test, TestingModule } from '@nestjs/testing';
+import { userController } from './user/user.controller';
 
 
-beforeEach(() => {
-  userService = new UserService();
-  userController = new UserController(userService);
-});
+/* Ce test vérifie que les données entrées par l'utilisateur pour se connecter sont du bon type. Dans cette situation, il y a 
+le type chaîne de caractère à tester pour les first name, last name, email, et password */
 
 
-  describe('root', () => {
-    it('Devrait retourner "Hello World!" à la racine du site', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+describe('User Controller', () => {
+    let controller: userController;
+
+    beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+    controllers: [userController],
+    }).compile();
+
+
+    controller = module.get<userController>(userController);
     });
-  });
 
+    
+    it('Le first name est bien un string', () => {
+    const firstName = 'John';
+    const result = controller.validateFirstName(firstName);
+    expect(typeof firstName).toBe('string');
+    });
+
+    it('Le last name est bien un string', () => {
+    const lastName = 'Doe';
+    const result = controller.validateLastName(lastName);
+    expect(typeof lastName).toBe('string');
+    });
+
+    it('L\'email est bien un string', () => {
+    const email = 'john.doe@example.com';
+    const result = controller.validateEmail(email);
+    expect(typeof email).toBe('string');
+    });
+
+    it('Le password est bien un string', () => {
+    const password = 'secret123';
+    const result = controller.validatePassword(password);
+    expect(typeof password).toBe('string');
+    });
 });
