bb1e4af90201969e13f3273f914d981dc2ad9a54
l_inzirillo <l_inzirillo@etu-webschoolfactory.fr>
2023-02-05 21:47:29 +0100

    add js for edit

---
+++ src/Front/edit.js
@@ -0,0 +1,6 @@
+document.querySelector('input[name="VideoName"]').value =
+  'Nouveau nom de la vidéo';
+document.querySelector('input[name="author"]').value = 'Nouvel auteur';
+document.querySelector('#sort-by').value = 'popular';
+document.querySelector('#category').value = 'gaming';
+document.querySelector('#duration').value = 'long';
